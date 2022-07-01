2c6092f03a7c40a5b51370596237105922b3de7e
Danyyl Dolynsky <dolynskydanyyl@gmail.com>
2022-07-01 21:48:24 +0300

    змінив split_text(text) на (main)
    передвинув new_text до for

--- Task 1 - Anagrams.py
+++ Task 1 - Anagrams.py
@@ -1,3 +1,32 @@
+def split_text(main):
+    words = main.split(' ')
+    new_text = []
+    for i in words:
+        new_text.append(reverse(i))
+    new_text = " ".join(new_text)
+    return new_text
+
+
+def reverse(word):
+    count = 0
+    numbers = []
+    wrong_letter = []
+
+    for letter in word:
+        count += 1
+        if not letter.isalpha():
+            word = word.replace(letter, '')
+            wrong_letter.append(letter)
+            numbers.append(count)
+
+    word = word[::-1]
+
+    for i in range(0, len(numbers)):
+        word = word[:numbers[i] - 1] + wrong_letter[i] + word[numbers[i] - 1::]
+
+    return word
+
+
 if __name__ == '__main__':
     cases = [
         ('abcd efgh', 'dcba hgfe'),
@@ -8,41 +37,6 @@ if __name__ == '__main__':
         ("your funct10n shou1d  work on @ny amount of w0rd5  and other  Ch@r@cter5",
          "ruoy ntcnu10f duoh1s  krow no @yn tnuoma fo d0rw5  dna rehto  re@t@crhC5"),
     ]
-
-
-    def split_text(text_1):
-        new_text = []
-        if len(text_1) > 1:
-            if text_1.isspace():
-                return reverse(text_1)
-            else:
-                words = text_1.split(' ')
-                for i in range(0, len(words)):
-                    new_text.append(reverse(words[i]))
-                new_text = " ".join(new_text)
-                return new_text
-        else:
-            return text_1
-
-
-    def reverse(word):
-        count = 0
-        numbers = []
-        wrong_letter = []
-
-        for letter in word:
-            count += 1
-            if not letter.isalpha():
-                word = word.replace(letter, '')
-                wrong_letter.append(letter)
-                numbers.append(count)
-
-        word = word[::-1]
-
-        for i in range(0, len(numbers)):
-            word = word[:numbers[i] - 1] + wrong_letter[i] + word[numbers[i] - 1::]
-
-        return word
 
     for text, reversed_text in cases:
         assert split_text(text) == reversed_text
